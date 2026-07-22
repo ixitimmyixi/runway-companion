430ac40f459b0f0f12364b1c496098c9eb5d93b8
ixitimmyixi <ixitimmyixi@gmail.com>
2026-07-22 14:31:07 -0500

    Update BufoEntity.java

--- src/main/java/com/example/companion/entity/BufoEntity.java
+++ src/main/java/com/example/companion/entity/BufoEntity.java
@@ -20,7 +20,10 @@ import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import org.jetbrains.annotations.Nullable;
 
+/** Bufo the companion: floats and hovers near the player like an Allay (Navi-style). */
 public class BufoEntity extends Animal implements FlyingAnimal {
+    private static final double MAX_ABOVE_PLAYER = 2.5;
+
     public BufoEntity(EntityType<? extends Animal> type, Level level) {
         super(type, level);
         this.moveControl = new FlyingMoveControl(this, 20, true);
@@ -64,24 +67,16 @@ public class BufoEntity extends Animal implements FlyingAnimal {
     @Override
     public void tick() {
         super.tick();
+        if (!level().isClientSide) {
+            Player p = level().getNearestPlayer(this, 32.0);
+            if (p != null && this.getY() > p.getY() + MAX_ABOVE_PLAYER) {
+                this.setPos(this.getX(), p.getY() + MAX_ABOVE_PLAYER, this.getZ());
+                var v = this.getDeltaMovement();
+                if (v.y > 0) this.setDeltaMovement(v.x, 0.0, v.z);
+            }
+        }
         if (level().isClientSide
                 && com.example.companion.client.BufoSpeakingState.isSpeaking()
                 && this.random.nextInt(4) == 0) {
             double px = getX() + (this.random.nextDouble() - 0.5) * 0.4;
             double py = getY() + 0.8;
-            double pz = getZ() + (this.random.nextDouble() - 0.5) * 0.4;
-            level().addParticle(ParticleTypes.NOTE, px, py, pz, this.random.nextDouble(), 0.0, 0.0);
-        }
-    }
-
-    @Override
-    public boolean isFood(ItemStack stack) {
-        return false;
-    }
-
-    @Nullable
-    @Override
-    public AgeableMob getBreedOffspring(ServerLevel level, AgeableMob partner) {
-        return null;
-    }
-}
